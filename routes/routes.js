02db3eb96bbaf15ffe2e4d1274019bd47f5d8e35
Kevin Steele <kevdsteele@gmail.com>
2019-09-14 19:32:40 -0400

    troubleshoot authentication

--- routes/routes.js
+++ routes/routes.js
@@ -20,7 +20,7 @@ app.get('/',  function (req, res) {
       };
     res.render("pages/auth", ejsObj)});
 
-app.get('/home', isAuthenticated , function (req, res) {
+app.get('/home', function (req, res) {
 var ejsObj = {
     pageTitle: "Home",
     user: req.user,
